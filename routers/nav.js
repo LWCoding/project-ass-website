4ff8ce154d03a10dd8980b17873b5ccdf23aa35e
Lucas <diamondmob01@gmail.com>
2022-02-20 01:21:38 -0600

    Updated biographies on homepage; haven't put explicit details yet

--- routers/nav.js
+++ routers/nav.js
@@ -1,5 +1,14 @@
 const express = require("express")
+const path = require("path")
 
 navRouter = express.Router()
+
+navRouter.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/contact.html"))
+})
+
+navRouter.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/about.html"))
+})
 
 module.exports = navRouter
